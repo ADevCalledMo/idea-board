da4a00444771d0d529fa446c330d57133ff605b6
ADevCalledMo <mozeswalker@outlook.com>
2022-08-17 07:50:38 +0100

    added routes to home and ideas

---
+++ src/components/Ideas.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Ideas = () => {
+  return (
+    <div>
+      <h1>Ideas Page: </h1>
+      <p>Here is where all your ideas are kept...</p>
+    </div>
+  );
+};
+
+export default Ideas;
