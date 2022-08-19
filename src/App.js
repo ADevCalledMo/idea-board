bc67f76ee06753297cff58e1ff562fbcde610198
ADevCalledMo <mozeswalker@outlook.com>
2022-08-19 09:14:52 +0100

    added a basic Login Screen

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Ideas from "./components/Ideas";
 import "./App.css";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogoutClick = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <Router>
       <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/ideas/Ongoing">Ideas</Link>
-            </li>
-            <li>
-              <Link to="/ideas/Archived">Archived Ideas</Link>
-            </li>
-          </ul>
-        </nav>
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/ideas/:stage" element={<Ideas />} />
-          </Routes>
-        </main>
+        {!isLoggedIn ? (
+          <h1> Please Login: </h1>
+        ) : (
+          <h1> Welcome to the Ideas Board </h1>
+        )}
+        <button onClick={handleLoginClick} disabled={isLoggedIn}>
+          Login
+        </button>
+        <button onClick={handleLogoutClick} disabled={!isLoggedIn}>
+          Logout
+        </button>
       </div>
+      {isLoggedIn && (
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/ideas/Ongoing">Ideas</Link>
+              </li>
+              <li>
+                <Link to="/ideas/Archived">Archived Ideas</Link>
+              </li>
+            </ul>
+          </nav>
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/ideas/:stage" element={<Ideas />} />
+            </Routes>
+          </main>
+        </div>
+      )}
     </Router>
   );
 }
--- src/components/Ideas.js
+++ src/components/Ideas.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 const Ideas = () => {
   const params = useParams();
@@ -8,6 +8,7 @@ const Ideas = () => {
     <div>
       <h1>{params.stage} Ideas Page: </h1>
       <p>Here is where all of your {params.stage} ideas are kept...</p>
+      <Link to="/">Back Home</Link>
     </div>
   );
 };
