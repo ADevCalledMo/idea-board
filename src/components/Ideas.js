4ea08f04b0fce38c399ce944d4a0e6d322b7c606
ADevCalledMo <mozeswalker@outlook.com>
2022-08-20 07:35:48 +0100

    Began form on Ongoing page

--- src/components/Ideas.js
+++ src/components/Ideas.js
@@ -8,7 +8,16 @@ const Ideas = () => {
     <div>
       <h1>{params.stage} Ideas Page: </h1>
       <p>Here is where all of your {params.stage} ideas are kept...</p>
+
       <Link to="/">Back Home</Link>
+
+      {params.stage === "Ongoing" && (
+        <form action="">
+          <label htmlFor="">Idea Name: </label>
+          <input type="text" />
+          <button>Submit</button>
+        </form>
+      )}
     </div>
   );
 };
