b4abde47456a0ffd80a95e6cdf2160c8723f9c83
ADevCalledMo <mozeswalker@outlook.com>
2022-08-20 11:34:42 +0100

    expanded on form

--- src/components/Ideas.js
+++ src/components/Ideas.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, Link } from "react-router-dom";
 
 const Ideas = () => {
   const params = useParams();
-  console.log(params);
+  const [ideas, setIdeas] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    setIdeas([
+      ...ideas,
+      {
+        id: ideas.length + 1,
+        name: name,
+        description: description,
+      },
+    ]);
+
+    setName("");
+    setDescription("");
+  };
+
   return (
     <div>
       <h1>{params.stage} Ideas Page: </h1>
@@ -12,11 +31,35 @@ const Ideas = () => {
       <Link to="/">Back Home</Link>
 
       {params.stage === "Ongoing" && (
-        <form action="">
-          <label htmlFor="">Idea Name: </label>
-          <input type="text" />
-          <button>Submit</button>
-        </form>
+        <>
+          <form onSubmit={handleFormSubmit}>
+            <label htmlFor="idea-name">Idea Name: </label>
+            <input
+              id="idea-name"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label htmlFor="idea-description">Description: </label>
+            <input
+              id="idea-description"
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <input type="submit" value="Add Idea" />
+          </form>
+
+          <div>
+            <ul>
+              {ideas.map((idea) => (
+                <li>
+                  `${idea.name}: ${idea.description}`<button>Delete</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
       )}
     </div>
   );
